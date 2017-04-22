6ccce3e6b07d21143a738bd3f20529d2039eb8ee
Victor Tolbert <victor.tolbert@gmail.com>
2017-04-22 08:48:17 -0400

    09 A Thread Should Be Assigned a Channel

--- tests/Feature/ParticipateInForumTest.php
+++ tests/Feature/ParticipateInForumTest.php
@@ -12,18 +12,19 @@ class ParticipateInForumTest extends TestCase
     /** @test */
     public function unauthenticated_users_may_not_add_replies()
     {
-        $this->expectException('Illuminate\Auth\AuthenticationException');
-        $this->post('/threads/1/replies', []);
+        $this->withExceptionHandling()
+            ->post('/threads/some-channel/1/replies', [])
+            ->assertRedirect('/login');
     }
 
     /** @test */
     public function an_authenticated_user_may_participate_in_forum_threads()
     {
-        $this->be($user = create('App\User'));
+        $this->signIn();
 
         $thread = create('App\Thread');
-
         $reply = make('App\Reply');
+
         $this->post($thread->path().'/replies', $reply->toArray());
 
         $this->get($thread->path())
