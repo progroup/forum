c32570caad92db07df645526240cadaf57b3bf0e
Victor Tolbert <victor.tolbert@gmail.com>
2017-04-23 11:36:28 -0400

    17 A User Can Filter Threads By Popularity

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -19,6 +19,11 @@
             'csrfToken' => csrf_token(),
         ]) !!};
     </script>
+    <style>
+        body { padding-bottom: 100px; }
+        .level { display: flex; align-items: center; }
+        .flex { flex: 1; }
+    </style>
 </head>
 <body style="padding-bottom: 100px">
     <div id="app">
@@ -53,6 +58,8 @@
                                     @if (auth()->check())
                                         <li><a href="/threads?by={{ auth()->user()->name }}">My Threads</a></li>
                                     @endif
+
+                                    <li><a href="/threads?popular=1">Popular Threads</a></li>
                                 </ul>
                             </a>
                         </li>
