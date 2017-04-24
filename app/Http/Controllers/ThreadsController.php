07d608a1439b211fe0fb449eeabc000731ac4fcc
Victor Tolbert <victor.tolbert@gmail.com>
2017-04-23 23:33:45 -0400

    24 Authorization With Policies

--- app/Http/Controllers/ThreadsController.php
+++ app/Http/Controllers/ThreadsController.php
@@ -82,6 +82,8 @@ class ThreadsController extends Controller
 
     public function destroy($channel, Thread $thread)
     {
+        $this->authorize('update', $thread);
+
         $thread->delete();
 
         if (request()->wantsJson()) {
